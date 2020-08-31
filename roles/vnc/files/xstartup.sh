999e628d3fed3ef0ae810b50c01942c23bc3e290
John Ratliff <jdratlif@globalnoc.iu.edu>
2020-08-31 13:41:29 +0000

    fix vnc config for mate

--- roles/vnc/files/xstartup.sh
+++ roles/vnc/files/xstartup.sh
@@ -1,10 +1,6 @@
-#! /bin/sh
+#!/bin/sh
 
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
-
-export XKL_XMODMAP_DISABLE=1
-
-xrdb -cpp /usr/bin/cpp $HOME/.Xresources
-xsetroot -solid grey
-i3 &
+/usr/bin/mate-session
+vncserver -kill $DISPLAY
