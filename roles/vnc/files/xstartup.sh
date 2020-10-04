920dfa11d293ba1983dcd4cfd68854ae65a6bbea
John Ratliff <jdratlif@globalnoc.iu.edu>
2020-10-04 17:03:12 -0400

    update vnc template

--- roles/vnc/files/xstartup.sh
+++ roles/vnc/files/xstartup.sh
@@ -2,5 +2,11 @@
 
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
+
+if [ -x /usr/bin/mate-session ]; then
 /usr/bin/mate-session
+elif [ -x /usr/bin/startxfce4 ]; then
+/usr/bin/startxfce4
+fi
+
 vncserver -kill $DISPLAY
