26b8a6cb6eee38eb5614fbbedaf232681a8d4538
John Ratliff <jdratlif@globalnoc.iu.edu>
2021-02-26 18:10:45 +0000

    add i3 to vnc config

--- roles/vnc/files/xstartup.sh
+++ roles/vnc/files/xstartup.sh
@@ -3,10 +3,12 @@
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
 
-if [ -x /usr/bin/mate-session ]; then
-/usr/bin/mate-session
+if [ -x /usr/bin/i3 ]; then
+    /usr/bin/i3
+elif [ -x /usr/bin/mate-session ]; then
+    /usr/bin/mate-session
 elif [ -x /usr/bin/startxfce4 ]; then
-/usr/bin/startxfce4
+    /usr/bin/startxfce4
 fi
 
 vncserver -kill $DISPLAY
